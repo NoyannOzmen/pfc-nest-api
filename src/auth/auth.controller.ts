f7149ef1d6dc7037677d07cc405965fa2c15e894
NoyannOzmen <noyann.ozmen@oclock.school>
2025-03-24 14:39:12 +0100

    Login route is now public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,11 +6,13 @@ import {
   Post 
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('connexion')
   signIn(@Body() signInDto: Record<string, any>) {
